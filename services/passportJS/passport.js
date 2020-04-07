a4fa5326e6e63ec47c0f42cb2fcd323d915e0454
AVER343 <ankushvermagb@gmail.com>
2020-04-07 09:03:52 +0530

    Prod

--- services/passportJS/passport.js
+++ services/passportJS/passport.js
@@ -2,11 +2,16 @@ const passport=require('passport')
 const config = require(`../../config/config`)
 const GoogleStrategy = require('passport-google-oauth20')
 const mongoose =require('mongoose')
+var server=`http://localhost:3000`
+if(process.env.NODE_ENV==='production')
+{
+    server=`http://mern-feedback-app.herokuapp.com/`
+}
 const User= mongoose.model('users')
 passport.use(new GoogleStrategy({
                 clientID:config.GoogleOAuth.web.client_id,
                 clientSecret:config.GoogleOAuth.web.client_secret,
-                callbackURL:'http://mern-feedback-app.herokuapp.com/auth/google/callback',
+                callbackURL:`${server}/auth/google/callback`,
                 proxy:true},
                 (accessToken,refreshToken,profile,done)=>
                     {
