e64db814ee4a1b0f56c6ccd1d512060787efa42f
AVER343 <ankushvermagb@gmail.com>
2020-04-09 06:22:05 +0530

    Auth + credits0101

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,12 +1,12 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
-    app.use(proxy('/api/*', 
+    app.use(proxy('api/*', 
         { target: 'http://localhost:5000/' }
     ));
-    app.use(proxy('/auth/google', 
+    app.use(proxy('auth/google', 
         { target: 'http://localhost:5000/' }
     ));
-    app.use(proxy('/api/stripe', 
+    app.use(proxy('api/stripe', 
         { target: 'http://localhost:5000/' }
     ));
 }
